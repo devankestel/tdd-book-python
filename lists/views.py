f8897a58665c28d84ec1a832f25063a1af99c989
Devan Kestel <devan.kestel@gmail.com>
2021-08-04 20:00:26 -0400

    Pass test with short circuit redirect on post.

--- lists/views.py
+++ lists/views.py
@@ -1,5 +1,5 @@
 from django.http.response import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
 
 from lists.models import Item
@@ -10,9 +10,6 @@ def home_page(request):
     if request.method == 'POST':
         new_item_text = request.POST.get('item_text', '')
         Item.objects.create(text=new_item_text)
-    else: 
-        new_item_text = ''
+        return redirect('/')
     
-    return render(request, 'home.html', {
-        'new_item_text': new_item_text
-    })
+    return render(request, 'home.html')
