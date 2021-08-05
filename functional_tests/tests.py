98aaa9dc3d6c9a0a5409833edfaaa420230c29ae
Devan Kestel <devan.kestel@gmail.com>
2021-08-05 17:10:43 -0400

    Switch to LiveServerTestCase

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,10 +1,11 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 import time
 import unittest
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
 
     def setUp(self) -> None:
         self.browser = webdriver.Firefox()
@@ -23,7 +24,7 @@ class NewVisitorTest(unittest.TestCase):
 
         # Edith has heard about a cool, new, online to-do app. 
         # She goes to check out its homepage
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
 
         # She notices the page title and header mention to-do lists.
         self.assertIn('Todo', self.browser.title)
@@ -71,6 +72,3 @@ class NewVisitorTest(unittest.TestCase):
         # She visits that URL - her todo list is still there.
 
         # Satisfied, she goes back to sleep. 
-
-if __name__ == '__main__':
-    unittest.main()
