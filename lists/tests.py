7647edc5d5576874c8ad4c39e9aa7ff36d24810a
Devan Kestel <devan.kestel@gmail.com>
2021-08-06 16:59:31 -0400

    Update expected post redirect location.

--- lists/tests.py
+++ lists/tests.py
@@ -26,7 +26,7 @@ class HomePageTest(TestCase):
         response = self.client.post('/', data={'item_text': 'A new list item'})
         
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/')
+        self.assertEqual(response['location'], '/lists/the-only-list-in-the-world')
     
     def test_only_saves_items_when_necessary(self):
         # We don't want to save on a get request, bc we aren't adding anything
