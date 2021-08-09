c29b52d88a731b99f7bfb23654266bacc6d23bce
Devan Kestel <devan.kestel@gmail.com>
2021-08-09 09:40:06 -0400

    Write failing unit for list model.

--- lists/tests.py
+++ lists/tests.py
@@ -5,7 +5,7 @@ from django.http import HttpRequest, response
 
 
 from lists.views import home_page
-from lists.models import Item
+from lists.models import Item, List
 
 # Create your tests here.
 
@@ -15,16 +15,25 @@ class HomePageTest(TestCase):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
 
-class ItemModelTest(TestCase):
+class ListAndItemModelsTest(TestCase):
 
     def test_saving_and_retrieving_items(self):
+
+        list_ = List()
+        list_.save()
+
         first_item = Item()
         first_item.text = 'The first (ever) list item'
+        firs_item.list = list_
         first_item.save()
 
         second_item = Item()
         second_item.text = 'Item the second'
+        second_item.list = list_
         second_item.save()
+
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, list_)
 
         saved_items = Item.objects.all()
         self.assertEqual(saved_items.count(), 2)
@@ -32,7 +41,9 @@ class ItemModelTest(TestCase):
         first_saved_item = saved_items[0]
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(first_saved_item.list, list_)
         self.assertEqual(second_saved_item.text, 'Item the second')
+        self.assertEqual(second_saved_item.list, list_)
 
 class ListViewTest(TestCase):
 
--- scratchpad.md
+++ scratchpad.md
@@ -4,7 +4,7 @@
 * [ ] support more than one list
     * [ ] adjust model so that items are associated with different lists
     * [ ] add unique URLs for each list
-    * [ ] add a URL for creating new list via POST
+    * [X] add a URL for creating new list via POST
     * [ ] add URLs for adding a new item to an existing list via POST
 * [X] clean up after functional test runs
 * [X] remove time.sleeps
