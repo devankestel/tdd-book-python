7e7a411758233c35e910b6812db2e95e8f156b4b
Devan Kestel <devan.kestel@gmail.com>
2021-08-15 02:07:50 -0400

    Passing unit with new redirect.

--- lists/views.py
+++ lists/views.py
@@ -19,4 +19,4 @@ def view_list(request, list_id):
 def new_list(request):
     my_list = List.objects.create()
     Item.objects.create(text=request.POST['item_text'], list=my_list)
-    return redirect('/lists/the-only-list-in-the-world/')
+    return redirect(f'/lists/{my_list.id}/')
